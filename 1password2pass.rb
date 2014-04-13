bfe786daa7824b65b3bfbb01aa7e254963aca2c8
Tobias Langhoff <tobias@langhoff.no>
2014-04-13 12:17:56 +0200

    Better error handling, streamline options, don't duplicate parsing

--- 1password2pass.rb
+++ 1password2pass.rb
@@ -10,8 +10,8 @@
 # in the 1Password Interchange File (1PIF) format.
 # 
 # Supports using the title (default) or URL as pass-name, depending on your
-# preferred organization. Also supports importing username, URL and notes, adding
-# them with `pass insert --multiline`; the username and URL are compatible with
+# preferred organization. Also supports importing metadata, adding them with
+# `pass insert --multiline`; the username and URL are compatible with
 # https://github.com/jvenant/passff.
 
 require "optparse"
@@ -19,83 +19,119 @@ require "ostruct"
 
 accepted_formats = [".txt", ".1pif"]
 
+# Default options
 options = OpenStruct.new
 options.force = false
-options.title = "title"
+options.name = :title
 options.group = "1password" # XXX
 options.notes = true
+options.meta = true
 
 optparse = OptionParser.new do |opts|
   opts.banner = "Usage: #{opts.program_name}.rb [options] filename"
-
-  opts.on("-f", "--force", "Overwrite existing records") { options.force = true }
-  opts.on("-d", "--default [GROUP]", "Place uncategorised records into GROUP") { |group| options.group = group }
-  opts.on("-n", "--name [PASS-NAME]", [:title, :location], "Select field to use as pass-name: title (default) or location (URL)") { |title| options.title = title }
-  opts.on("-u", "--username", "Import username and add it below the password in passff compatible format") { options.username = true }
-  opts.on("-l", "--location", "Import location (URL) and add it below the password in passff compatible format") { options.location = true }
-  opts.on("-i", "--ignore-notes", "Do not import notes (default behavior is to add notes below the password)") { options.notes = false }
+  opts.on("-f", "--force", "Overwrite existing passwords") { options.force = true }
+  opts.on("-d", "--default [FOLDER]", "Place passwords into FOLDER") { |group| options.group = group }
+  opts.on("-n", "--name [PASS-NAME]", [:title, :url], "Select field to use as pass-name: title (default) or URL") { |name| options.name = name }
+  opts.on("-m", "--[no-]meta", "Import metadata and insert it below the password") { |meta| options.meta = meta }
   opts.on_tail("-h", "--help", "Display this screen") { puts opts; exit }
 
   begin 
     opts.parse!
   rescue OptionParser::InvalidOption
-    puts optparse
+    $stderr.puts optparse
     exit
   end
 end
 
 # Check for a filename
 filename = ARGV.pop
-unless filename and accepted_formats.include? File.extname(filename)
-  puts optparse
-  exit 1
+unless filename
+  abort optparse.to_s
+end
+unless accepted_formats.include? File.extname filename
+  abort "Supported file types are comma and tab delimited .txt files, and .1pif files."
 end
 
-# comma or tab delimited text
+passwords = []
+
+# Parse comma or tab delimited text
 if File.extname(filename) =~ /.txt/i
   require "csv"
 
+  # Very simple way to guess the delimiter
   delimiter = ""
   File.open(filename) do |file|
     first_line = file.readline
-
-    # Very simple way to guess the delimiter
     if first_line =~ /,/
       delimiter = ","
     elsif first_line =~ /\t/
       delimiter = "\t"
     else
-      puts "File is neither comma nor tab delimited. Aborting."
-      exit 1
+      abort "Supported file types are comma and tab delimited .txt files, and .1pif files."
     end
   end
 
-  # Import the data
-  CSV.foreach(filename, {:col_sep => delimiter, :headers => true}) do |pass|
-    IO.popen("pass insert #{"-f " if options.force}-m '#{(options.group + "/") if options.group}#{pass[options.title]}'", "w") do |io|
-      io.puts pass["password"]
-      io.puts "login: #{pass["username"]}" if options.username and pass["username"] != ""
-      io.puts "url: #{pass["location"]}" if options.location and pass["location"] != ""
-      io.puts pass["notes"] if options.notes
-    end
+  # Import CSV/TSV
+  CSV.foreach(filename, {:col_sep => delimiter, :headers => true, :header_converters => :symbol}) do |entry|
+    pass = {}
+    pass[:name] = "#{(options.group + "/") if options.group}#{entry[options.name]}"
+    pass[:title] = entry[:title]
+    pass[:password] = entry[:password]
+    pass[:login] = entry[:username]
+    pass[:url] = entry[:url]
+    pass[:notes] = entry[:notes]
+    passwords << pass
   end
-# 1PIF format
+# Parse 1PIF
 elsif File.extname(filename) =~ /.1pif/i
   require "json"
 
   # 1PIF is almost JSON, but not quite
   pif = "[#{File.open(filename).read}]"
   pif.gsub!(/^\*\*\*.*\*\*\*$/, ",")
-  pif = JSON.parse(pif)
+  pif = JSON.parse(pif, {:symbolize_names => true})
 
-  pif.each do |pass|
-    if pass["typeName"] == "webforms.WebForm"
-      IO.popen("pass insert #{"-f " if options.force}-m '#{(options.group + "/") if options.group}#{pass[options.title]}'", "w") do |io|
-        io.puts pass["secureContents"]["fields"].each { |field| break field["value"] if field["name"] == "password" } # XXX
-        io.puts "login: " + pass["secureContents"]["fields"].each { |field| break field["value"] if field["name"] == "username" } # XXX
-        io.puts "url: #{pass["location"]}" if options.location and pass["location"] != ""
-        io.puts pass["secureContents"]["notesPlain"] if options.notes
-      end
+  options.name = :location if options.name == :url
+
+  # Import 1PIF
+  pif.each do |entry|
+    next unless entry[:typeName] == "webforms.WebForm"
+    pass = {}
+    pass[:name] = "#{(options.group + "/") if options.group}#{entry[options.name]}"
+    pass[:title] = entry[:title]
+    pass[:password] = entry[:secureContents][:fields].each { |field| break field[:value] if field[:name] == "password" }
+    pass[:login] = entry[:secureContents][:fields].each { |field| break field[:value] if field[:name] == "username" }
+    pass[:url] = entry[:location]
+    pass[:notes] = entry[:secureContents][:notesPlain]
+    passwords << pass
+  end
+end
+
+puts "Read #{passwords.length} passwords."
+
+errors = []
+# Save the passwords
+passwords.each do |pass|
+  IO.popen("pass insert #{"-f " if options.force}-m '#{pass[:name]}' > /dev/null", "w") do |io|
+    io.puts pass[:password]
+    if options.meta
+      io.puts "login: #{pass[:login]}" unless pass[:login].to_s.empty?
+      io.puts "url: #{pass[:url]}" unless pass[:url].to_s.empty?
+      io.puts pass[:notes] unless pass[:notes].to_s.empty?
     end
   end
+  if $? == 0
+    puts "Imported #{pass[:name]}"
+  else
+    $stderr.puts "ERROR: Failed to import #{pass[:name]}"
+    errors << pass
+  end
+end
+
+if errors.length > 0
+  $stderr.print "Failed to import "
+  $stderr.puts errors.map {|e| e[:name]}.join ", "
+  $stderr.puts "Check the errors. Make sure these passwords do not already exist. If "\
+       "you're sure you want to overwrite them with the new import, try "\
+       "again with --force."
 end
